2423f3524dc9b360ff5e047ebd10c75d02ad4081
Rachel Killackey <rkillac1@gmail.com>
2014-07-07 15:18:15 -0500

    initial commit

---
+++ map-data.js
@@ -0,0 +1,87 @@
+var jobData = [	
+	{
+		name: "buffalo.edu",
+		radius: 10,
+		time: 57.80761574074074,
+		latitude: 42.9047,
+		longitude: -78.8494
+	},
+	{
+		name: "tier2",
+		time: 437.185625,
+		radius: 10,
+		latitude: 41.8819,
+		longitude: -87.6278	
+	},
+	{
+        name: "unl.edu",
+		radius: 10,
+        time: 1054.89125,
+		latitude: 40.8097,
+		longitude: -96.6753	
+	},
+	{
+        name: "purdue.edu",
+		radius: 10,
+        time: 192.94825231481482,
+		latitude: 40.4240,
+		longitude: -86.9290	
+	},
+	{
+        name: "ucdavis.edu",
+		radius: 10,
+        time: 40.76177083333333,
+		latitude: 38.5400,
+		longitude: -121.7500	
+	},
+	{
+        name: "aglt2.org",
+		radius: 10,
+        time: 161.65851851851852,
+		latitude: 42.2828,
+		longitude: -83.7347	
+	},
+	{
+        name: "wisc.edu",
+		radius: 10,
+        time: 147.29196759259258,
+		latitude: 43.0750,
+		longitude: -89.4172
+	},
+	{
+        name: "northwestern.edu",
+		radius: 10,
+        time: 53.48828703703703,
+		latitude: 42.0464,
+		longitude: -87.6947
+	},
+	{
+        name: "smu.edu",
+		radius: 10,
+        time: 149.71890046296298,
+		latitude: 32.8441,
+		longitude: -96.7849
+	},
+	{
+        name: "bnl.gov",
+		radius: 10,
+        time: 15.867118055555556,
+		latitude: 40.8694,
+		longitude: -72.8867
+	},
+	{
+        name: "iu.edu",
+		radius: 10,
+        time: 183.15769675925924,
+		latitude: 39.1768,
+		longitude: -86.5197
+	},
+	{
+        name: "nd.edu",
+		radius: 10,
+        time: 69.01653935185185,
+		latitude: 41.7030,
+		longitude: -86.2390	
+	}
+	
+	];
